f1fb0d03d472b0ec0fbdeae74ad7354cc2844167
Dinesh Purohit <dp3867@yahoo.com>
2013-11-22 11:01:34 -0800

    Heap test

--- heap.js
+++ heap.js
@@ -5,7 +5,7 @@
 ******************************************************/
 
 
-var DS.Heap = function(){
+DS.Heap = function(){
 
 }
 
@@ -15,10 +15,25 @@ DS.Heap.prototype = {
  createHeap : function(arr){
 	
 	// go through the array and fix the heap - we keep 0th element empty
-	for(var i=arr.length;i>=1;i--){
-	  var parent = Math.floor(i/2);
-	  console.log();
+	for(var i=arr.length-1;i>=0;i--){
+	//console.log(i);
+	 var parent = Math.floor(i/2);
+	 var swap = true;
+	  //if parent is greater than child swap them
+	 // console.log("Parent of ", arr[i], " is ", arr[parent], " at location", parent);
+	while(swap){
+	//console.log(parent);
+	 if(arr[parent] > arr[i]){
+	  	var temp = arr[parent];
+		arr[parent] = arr[i];
+		arr[i] = temp;
+		parent = Math.floor(parent / 2);
+	 }else{
+		swap = false;
+		}
+         }
 	}
+	return arr;
  }
 
 }
