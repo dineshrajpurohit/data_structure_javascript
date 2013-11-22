788b1fb4224a5cb5c301e28d9014e0ed6b9e5235
Dinesh Purohit <dp3867@yahoo.com>
2013-11-21 22:02:48 -0800

    Heap File

---
+++ heap.js
@@ -0,0 +1,26 @@
+/******************************************************
+* Heap Data Structure implementation (Only Integers)
+*
+* Dinesh Purohit
+******************************************************/
+
+
+var DS.Heap = function(){
+
+}
+
+DS.Heap.prototype = {
+
+// creating a heap
+ createHeap : function(arr){
+	
+	// go through the array and fix the heap - we keep 0th element empty
+	for(var i=arr.length;i>=1;i--){
+	  var parent = Math.floor(i/2);
+	  console.log();
+	}
+ }
+
+}
+
+
